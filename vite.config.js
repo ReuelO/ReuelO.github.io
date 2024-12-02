83bd0756357f8eb94968e631885f9c45ab9b0247
Reuel Omwenga <50078634+ReuelO@users.noreply.github.com>
2024-12-02 14:21:33 +0300

    set up gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
-import ghPages from "vite-plugin-gh-pages";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/reuelo.github.io/",
-  plugins: [react(), ghPages()],
+  plugins: [react()],
 });
