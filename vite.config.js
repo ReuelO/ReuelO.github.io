14b4791ed452549e1e66510f6e551eee5cf9a8e3
Reuel Omwenga <50078634+ReuelO@users.noreply.github.com>
2024-12-02 15:23:01 +0300

    fixed gh-pages issue

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "tailwindcss";
 import { defineConfig } from "vite";
-import ghPages from "vite-plugin-gh-pages";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/reuelo.github.io/",
-  plugins: [react(), ghPages()],
+  plugins: [react()],
   css: {
     postcss: {
       plugins: [tailwindcss()],
